d70009023c418540b0d8124d2a1cbf36442fb340
Adi Tanase <Adi Tanase>
2016-02-25 09:55:56 +0100

    date picker object and restrictions

---
+++ BatesMotel/Models/Interval.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BatesMotel.Models
+{
+    public class Interval
+    {
+        public string CheckIn_Date { get; set; }
+        public string CheckOut_Date { get; set; }
+    }
+}
